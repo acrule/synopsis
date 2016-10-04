80854f92e6e62b4e5b5dad97f61688386ac2256a
acrule <acrule@gmail.com>
2016-10-03 17:49:29 -0700

    updated index to include iterative template

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 
+var index = require('./routes/index');
+
+
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function (req, res) {
-    res.render('index', {
-        title: "Biopsy"
-    });
-});
+app.use('/', index);
+
+// app.get('/', function (req, res) {
+//     res.render('index', {
+//         title: "Biopsy"
+//     });
+// });
 
 app.use('/public', express.static('public'));
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      'title': 'Biopsy',
+      'features':[
+          {'text':'An amazing thing'
+          },
+          {'text':'Another amazing thing'
+          },
+          {'text':'Yet a third amazing thing'
+          },
+      ]
+  });
 });
 
 module.exports = router;
